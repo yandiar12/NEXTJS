79a8955e7337bddc2539eedb3de3d1b46f5df477
yandiar12 <yandiar.rohman@gmail.com>
2021-11-17 11:41:26 +0700

    modify title

--- app-list/components/layout.js
+++ app-list/components/layout.js
@@ -1,9 +1,14 @@
+import Head from 'next/head'
 import Footer from './footer';
 import Navbar from './navbar';
 
 const Layout = ({ children }) => {
   return (
     <div className="wrapper">
+      <Head>
+        <title>App List</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
       <Navbar />
         { children }
       <Footer />  
--- app-list/pages/index.js
+++ app-list/pages/index.js
@@ -1,14 +1,9 @@
-import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Ninja List</title>
-        <meta name="keywords" content="ninjas" />
-      </Head>
       <div>
         <h1 className={styles.title}>Homepage</h1>
         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe officia, ea iure odio explicabo voluptatibus, molestiae numquam dolores quibusdam doloremque itaque cumque fuga aut minus dolore illo deserunt blanditiis tempora?</p>
