3ecb26fcd567ec384d616ae02739f772ec801772
yandiar12 <yandiar.rohman@gmail.com>
2021-11-17 11:22:50 +0700

    Details Page

--- app-list/pages/content/[id].js
+++ app-list/pages/content/[id].js
@@ -1,6 +1,35 @@
-const Details = () => {
+export const getStaticPaths = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  const paths = data.map(post => {
+    return {
+      params: { id: post.id.toString() }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/' + id);
+  const data = await res.json();
+
+  return {
+    props: { post: data }
+  }  
+}
+
+const Details = ({ post }) => {
   return (
-    <h1>Details page</h1>
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
   );
 }
  
